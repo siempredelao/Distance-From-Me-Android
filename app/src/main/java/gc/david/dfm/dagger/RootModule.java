b0cafa7b3de3f9668ad33b7ae7935df7326bfb3d
David Aguiar <siempredelao@gmail.com>
2015-08-25 20:17:40 +0200

    Adding root module

---
+++ app/src/main/java/gc/david/dfm/dagger/RootModule.java
@@ -0,0 +1,47 @@
+package gc.david.dfm.dagger;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import gc.david.dfm.DFMApplication;
+import gc.david.dfm.model.DaoSession;
+import gc.david.dfm.ui.MainActivity;
+import gc.david.dfm.ui.ShowInfoActivity;
+
+@Module
+public class RootModule {
+
+    private final DFMApplication application;
+
+    public RootModule(DFMApplication application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    public DFMApplication getApplication() {
+        return application;
+    }
+
+    @Provides
+    @Singleton
+    public Context getContext() {
+        return application.getApplicationContext();
+    }
+
+    @Provides
+    @Singleton
+    public DaoSession getDaoSession(DFMApplication application) {
+        return application.getDaoSession();
+    }
+
+    @Provides
+    @Singleton
+    public PackageManager getPackageManager(DFMApplication application) {
+        return application.getPackageManager();
+    }
+}
