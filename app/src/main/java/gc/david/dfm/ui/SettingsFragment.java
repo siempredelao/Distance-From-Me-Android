eeced040d11dd7033c2df2028a927b63413c6069
David Aguiar <siempredelao@gmail.com>
2017-01-16 14:43:06 +0100

    Providing DaoSession in SettingsFragment

--- app/src/main/java/gc/david/dfm/ui/SettingsFragment.java
+++ app/src/main/java/gc/david/dfm/ui/SettingsFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
+import javax.inject.Inject;
+
 import gc.david.dfm.DFMApplication;
 import gc.david.dfm.DFMPreferences;
 import gc.david.dfm.R;
+import gc.david.dfm.dagger.DaggerRootComponent;
+import gc.david.dfm.dagger.RootModule;
 import gc.david.dfm.logger.DFMLogger;
 import gc.david.dfm.model.DaoSession;
 import gc.david.dfm.model.Distance;
@@ -18,9 +22,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     private static final String TAG = SettingsFragment.class.getSimpleName();
 
+    @Inject
+    protected DaoSession daoSession;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
+
+        DaggerRootComponent.builder()
+                           .rootModule(new RootModule((DFMApplication) getActivity().getApplication()))
+                           .build()
+                           .inject(this);
 
         final Preference bbddPreference = findPreference(DFMPreferences.CLEAR_DATABASE_KEY);
         bbddPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
@@ -29,15 +41,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 DFMLogger.logMessage(TAG, "onPreferenceClick delete entries");
 
                 // TODO hacerlo en segundo plano
-                getApplicationDaoSession().deleteAll(Distance.class);
-                getApplicationDaoSession().deleteAll(Position.class);
+                daoSession.deleteAll(Distance.class);
+                daoSession.deleteAll(Position.class);
                 toastIt(getString(R.string.toast_distances_deleted), getActivity().getApplicationContext());
                 return false;
             }
         });
-    }
-
-    private DaoSession getApplicationDaoSession() {
-        return ((DFMApplication) getActivity().getApplicationContext()).getDaoSession();
     }
 }
