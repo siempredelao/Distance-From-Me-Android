3035347f7913463297e04b6fe521d307b264daab
David Aguiar <siempredelao@gmail.com>
2019-11-08 16:54:10 +0100

    Migrate migration package to Kotlin

--- app/src/main/java/gc/david/dfm/migration/UpgradeHelper.kt
+++ app/src/main/java/gc/david/dfm/migration/UpgradeHelper.kt
@@ -14,38 +14,33 @@
  * limitations under the License.
  */
 
-package gc.david.dfm.migration;
+package gc.david.dfm.migration
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-
-import org.greenrobot.greendao.database.Database;
-
-import gc.david.dfm.model.DaoMaster;
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import gc.david.dfm.model.DaoMaster
+import org.greenrobot.greendao.database.Database
 
 /**
  * A simple helper which determines which migration (if any) is required to be
  * applied when a database is opened.
- * <p/>
+ *
+ *
  * Created by David on 29/10/2014.
  */
-public class UpgradeHelper extends DaoMaster.OpenHelper {
-
-    public UpgradeHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
-        super(context, name, factory);
-    }
+class UpgradeHelper(
+        context: Context,
+        name: String,
+        factory: SQLiteDatabase.CursorFactory?
+) : DaoMaster.OpenHelper(context, name, factory) {
 
     /**
      * Apply the appropriate migrations to update the database.
      */
-    @Override
-    public void onUpgrade(Database db, int oldVersion, int newVersion) {
-        switch (newVersion) {
-            case 2:
-                new MigrateV1ToV2().applyMigration(db, oldVersion);
-                break;
-            default:
-                break;
+    override fun onUpgrade(database: Database, oldVersion: Int, newVersion: Int) {
+        when (newVersion) {
+            2 -> MigrateV1ToV2().applyMigration(database, oldVersion)
+            else -> { /* nothing*/ }
         }
     }
 }
