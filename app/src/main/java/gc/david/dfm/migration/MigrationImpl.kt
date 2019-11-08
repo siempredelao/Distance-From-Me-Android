e9e1eb1da01e62666c613f458eb715ffd0a049bc
David Aguiar <siempredelao@gmail.com>
2019-11-08 16:54:10 +0100

    Migrate migration package to Kotlin

--- app/src/main/java/gc/david/dfm/migration/MigrationImpl.kt
+++ app/src/main/java/gc/david/dfm/migration/MigrationImpl.kt
@@ -14,18 +14,17 @@
  * limitations under the License.
  */
 
-package gc.david.dfm.migration;
+package gc.david.dfm.migration
 
-import org.greenrobot.greendao.database.Database;
-
-import static dagger.internal.Preconditions.checkNotNull;
+import org.greenrobot.greendao.database.Database
 
 /**
  * An abstract base class with the basic implementation to promote reuse.
- * <p/>
+ *
+ *
  * Created by David on 29/10/2014.
  */
-public abstract class MigrationImpl implements Migration {
+abstract class MigrationImpl : Migration {
 
     /**
      * A helper method which helps the migration prepare by passing the call up the chain.
@@ -33,29 +32,24 @@ public abstract class MigrationImpl implements Migration {
      * @param db             Database.
      * @param currentVersion Version before migration.
      */
-    protected void prepareMigration(final Database db, final int currentVersion) {
-        checkNotNull(db, "Database cannot be null");
-        if (currentVersion < 1) {
-            throw new IllegalArgumentException("Lowest supported schema version is 1, unable to prepare for migration" +
-                                               " from version: "
-                                               + currentVersion);
+    protected fun prepareMigration(db: Database, currentVersion: Int) {
+        require(currentVersion >= 1) {
+            ("Lowest supported schema version is 1, unable to prepare for migration from version: $currentVersion")
         }
 
         if (currentVersion < getSourceVersion()) {
-            final Migration previousMigration = getPreviousMigration();
+            val previousMigration = getPreviousMigration()
 
             if (previousMigration == null) {
                 // This is the first migration
-                if (currentVersion != getSourceVersion()) {
-                    throw new IllegalStateException("Unable to apply migration as Version: "
-                                                    + currentVersion
-                                                    + " is not suitable for this Migration.");
+                check(currentVersion == getSourceVersion()) {
+                    ("Unable to apply migration as Version: $currentVersion is not suitable for this Migration.")
                 }
             }
-            if (previousMigration.applyMigration(db, currentVersion) != getSourceVersion()) {
+            check(previousMigration!!.applyMigration(db, currentVersion) == getSourceVersion()) {
                 // For all other migrations ensure that after the earlier
                 // migration has been applied the expected version matches
-                throw new IllegalStateException("Error, expected migration parent to update database to appropriate version");
+                "Error, expected migration parent to update database to appropriate version"
             }
         }
     }
